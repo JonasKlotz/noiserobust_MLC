568621b2be4e31345c66d62f95a6ff6375c697aa
Jonas Klotz <jonasklotz@hotmail.de>
2022-07-17 12:37:32 +0200

    Splitted runs for cluster others

---
+++ cluster_instructions/lamp_bce.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#SBATCH -J lamp_bce	# Job Name
+
+#SBATCH --nodes=2               # Anzahl Knoten N
+#SBATCH --ntasks-per-node=5     # Prozesse n pro Knoten
+#SBATCH --ntasks-per-core=5	  # Prozesse n pro CPU-Core
+#SBATCH --mem=15G              # 500MiB resident memory pro node
+
+##Max Walltime vorgeben:
+#SBATCH --time=40:00:00 # Erwartete Laufzeit
+
+## AUf GPU Rechnen
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:tesla:1                      # Use 1 GPU per node
+
+
+
+#SBATCH -o logs/logfile_lamp_bce                # send stdout to outfile
+#SBATCH -e logs/errfile_lamp_bce                  # send stderr to errfile
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate rs_3.8
+
+
+echo Start
+
+# Parameters for running
+model=("lamp")
+loss=("bce")
+optim=("adam"  "sgd")
+d_model=(50 300)
+learning_rates=(0.001  0.005)
+noises=(0.1 0.3 0.5 0.7)
+
+
+for m in ${model[@]}; do
+	for l in ${loss[@]}; do
+		for o in ${optim[@]}; do
+			for d in ${d_model[@]}; do
+				for lr in ${learning_rates[@]}; do
+						for n in ${noises[@]}; do
+              add_noise="-model ${m} -loss ${l} -optim ${o} -d_model ${d} -lr ${lr} -add_noise ${n}"
+              python3 src/main.py add_noise
+              sub_noise="-model ${m} -loss ${l} -optim ${o} -d_model ${d} -lr ${lr} -sub_noise ${n}"
+              python3 src/main.py sub_noise
+              balanced="-model ${m} -loss ${l} -optim ${o} -d_model ${d} -lr ${lr} -add_noise ${n} -sub_noise ${n}"
+              python3 src/main.py balanced
+          done
+				done
+			done
+		done
+	done
+done
+
+
